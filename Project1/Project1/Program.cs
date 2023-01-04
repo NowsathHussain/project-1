ab33183c09acb3e9d4e579eaa645ab0df2aab3f4
Nowsath <nowsath598@gmail.com>
2023-01-04 12:49:21 +0530

    SimpleInterested Completed

--- Project1/Project1/Program.cs
+++ Project1/Project1/Program.cs
@@ -13,12 +13,19 @@ namespace Project1
             int a = 10;
             int b = 20;
             Console.WriteLine(Sum(a,b));
+            Console.WriteLine(Interest(10000,5,10));
 
         }
         #region functions
         public static int Sum(int a,int b)
         {
             return a + b;
+        }
+        #endregion
+        #region simPleInterest
+        public static double Interest(int a, int b, int c)
+        {
+            return a * b * c / 100.0;
         }
         #endregion
     }
